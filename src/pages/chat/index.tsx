933bf96498f869b29ed086e4f92ab0e99071ab5f
Ashutosh Maurya <ashutoshm9695@gmail.com>
2024-05-27 13:06:20 +0530

    chat added

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,3 +1,34 @@
+import { useEffect } from 'react';
+// @mui
+import { Card, Container } from '@mui/material';
+// redux
+// import { useDispatch } from '../../redux/store';
+// import { getConversations, getContacts } from '../../redux/slices/chat';
+// routes
+
+// hooks
+
+// sections
+import { ChatSidebar, ChatWindow } from '../../components/chat';
+
+// ----------------------------------------------------------------------
+
 export default function Chat() {
-  return <div></div>;
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getConversations());
+  //   dispatch(getContacts());
+  // }, [dispatch]);
+
+  return (
+    <>
+      <Container>
+        <Card sx={{ height: '72vh', display: 'flex' }}>
+          <ChatSidebar />
+          <ChatWindow />
+        </Card>
+      </Container>
+    </>
+  );
 }
